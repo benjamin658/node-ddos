ffe53c88b4e993f4f77eb5e013f3e91880f408af
Ben Hu <benjamin658gae@gmail.com>
2015-09-08 17:06:53 +0800

    add alert mail

---
+++ config.js
@@ -0,0 +1,4 @@
+'use strict';
+/**
+ * Created by Ben Hu on 15/9/8.
+ */
--- test/test-express.js
+++ test/test-express.js
@@ -2,8 +2,11 @@ var http = require('http');
 var express = require('express')
 var request = require('request');
 var response = require('response')
-var Ddos = require('../'); var ddos = new Ddos({burst:3,limit:4,testmode:true})
-var app = express(); app.use(ddos.express)
+var Ddos = require('../');
+var ddos = new Ddos({burst: 3, limit: 4, testmode: true})
+var app = express();
+app.enable('trust proxy');
+app.use(ddos.express)
 var server = http.createServer(app);
 
 var QL = require('queuelib')
@@ -11,89 +14,89 @@ var q = new QL;
 
 server.listen(5050);
 
-var a = function(req,res,next) {
+var a = function (req, res, next) {
     next()
 }
-var b = function(req,res,next) {
+var b = function (req, res, next) {
     // some more random middleware
     next()
 }
-var c = function(req,res,next) {
-    response.json({foo:"bar"}).pipe(res)
+var c = function (req, res, next) {
+    response.json({foo: "bar"}).pipe(res)
 }
-app.get('/article',a,b,c);
+app.get('/article', a, b, c);
 
 var tape = require('tape')
-tape('count and expiry test', function(t) {
+tape('count and expiry test', function (t) {
     t.plan(7)
     q.series([
-        function(lib) {
-        request.get({url:'http://localhost:5050/article', json:true}, function(err, resp, body) {
-            var key = Object.keys(ddos.table)[0]
-            t.deepEqual(ddos.table[key], {count:1, expiry:1})
-            lib.done()
-        })
-        },
-        function(lib) {
-        request.get({url:'http://localhost:5050/article', json:true}, function(err, resp, body) {
-            var key = Object.keys(ddos.table)[0]
-            t.deepEqual(ddos.table[key], {count:2, expiry:1})
-            lib.done()
-        })
-        },
-        function(lib) {
-        request.get({url:'http://localhost:5050/article', json:true}, function(err, resp, body) {
-            var key = Object.keys(ddos.table)[0]
-            t.deepEqual(ddos.table[key], {count:3, expiry:1})
-            lib.done()
-        })
-        },
-        function(lib) {
-        request.get({url:'http://localhost:5050/article', json:true}, function(err, resp, body) {
-            var key = Object.keys(ddos.table)[0]
-            t.deepEqual(ddos.table[key], {count:4, expiry:2})
-            lib.done()
-        })
-        },
-        function(lib) {
-        request.get({url:'http://localhost:5050/article', json:true}, function(err, resp, body) {
-            var key = Object.keys(ddos.table)[0]
-            t.deepEqual(ddos.table[key], {count:5, expiry:4})
-            t.equal(resp.statusCode, 500, 'should be 500')        
-            t.equal(body.count, 5, 'should be 5')        
-            lib.done()
-        })
-        },
-        function(lib) {
-            lib.done()
-            t.end()
-        }
-    ],
-    100)
+            function (lib) {
+                request.get({url: 'http://localhost:5050/article', json: true}, function (err, resp, body) {
+                    var key = Object.keys(ddos.table)[0]
+                    t.deepEqual(ddos.table[key], {count: 1, expiry: 1, alert: false})
+                    lib.done()
+                })
+            },
+            function (lib) {
+                request.get({url: 'http://localhost:5050/article', json: true}, function (err, resp, body) {
+                    var key = Object.keys(ddos.table)[0]
+                    t.deepEqual(ddos.table[key], {count: 2, expiry: 1, alert: false})
+                    lib.done()
+                })
+            },
+            function (lib) {
+                request.get({url: 'http://localhost:5050/article', json: true}, function (err, resp, body) {
+                    var key = Object.keys(ddos.table)[0]
+                    t.deepEqual(ddos.table[key], {count: 3, expiry: 1, alert: false})
+                    lib.done()
+                })
+            },
+            function (lib) {
+                request.get({url: 'http://localhost:5050/article', json: true}, function (err, resp, body) {
+                    var key = Object.keys(ddos.table)[0]
+                    t.deepEqual(ddos.table[key], {count: 4, expiry: 2, alert: false})
+                    lib.done()
+                })
+            },
+            function (lib) {
+                request.get({url: 'http://localhost:5050/article', json: true}, function (err, resp, body) {
+                    var key = Object.keys(ddos.table)[0]
+                    t.deepEqual(ddos.table[key], {count: 5, expiry: 4, alert: false})
+                    t.equal(resp.statusCode, 500, 'should be 500')
+                    t.equal(body.count, 5, 'should be 5')
+                    lib.done()
+                })
+            },
+            function (lib) {
+                lib.done()
+                t.end()
+            }
+        ],
+        100)
 })
 
-tape('then', function(t) {
+tape('then', function (t) {
     t.plan(1)
-    setTimeout(function() {
-        request.get({url:'http://localhost:5050/article', json:true}, function(err, resp, body) {
+    setTimeout(function () {
+        request.get({url: 'http://localhost:5050/article', json: true}, function (err, resp, body) {
             var key = Object.keys(ddos.table)[0]
             // should start at {count:5, expiry:1} since 4 - 3 = 1
             // after a request, it should penalize for being over burst, which means
             // expiry goes to 2 
-            t.deepEqual(ddos.table[key], {count:6, expiry:2})
+            t.deepEqual(ddos.table[key], {count: 6, expiry: 2, alert: false})
         })
-    },3100)
+    }, 3100)
 })
 
-tape('finally',function(t) {
+tape('finally', function (t) {
     t.plan(2)
-    setTimeout(function() {
+    setTimeout(function () {
         t.deepEqual(ddos.table, {})
-        request.get({url:'http://localhost:5050/article', json:true}, function(err, resp, body) {
+        request.get({url: 'http://localhost:5050/article', json: true}, function (err, resp, body) {
             var key = Object.keys(ddos.table)[0]
-            t.deepEqual(ddos.table[key], {count:1,expiry:1})
+            t.deepEqual(ddos.table[key], {count: 1, expiry: 1, alert: false})
             server.close()
             ddos.stop()
         })
-    },2100)
+    }, 2100)
 })
